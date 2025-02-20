ec24b8a5b3fee7dff5767a3e7f879fe2cd351645
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2025-02-20 22:57:44 +0100

    Update releases.nix

--- pkgs/development/cuda-modules/cudatoolkit/releases.nix
+++ pkgs/development/cuda-modules/cudatoolkit/releases.nix
@@ -103,6 +103,6 @@
   "12.8" = {
     version = "12.8.0";
     url = "https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda_12.8.0_570.86.10_linux.run";
-    sha256 = "sha256-1y4m0qh886zjck3zd2jf2q4fqa6vp40iskwjqhv4wk6rsvf6f231=";
+    sha256 = "sha256-1y4m0qh886zjck3zd2jf2q4fqa6vp40iskwjqhv4wk6rsvf6f231";
   };
 }
